4249bf37230b1985f4a1b65f5eba3dccbbacf681
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-14 14:48:17 +0100

    menu

---
+++ nanau-purple/pages/include/header.php
@@ -0,0 +1,15 @@
+
+          <header class="menu">
+              <h1>Nanau_purple</h1>
+
+              <ul>
+                  <li><a href="Page.bijoux.php">Action</a></li>
+                  <li><a href="Page.piercing.php">Aventure</a></li>
+                  <li><a href="./tattoos.php">Drame</a></li>
+                  <li><a href="Page.tarifs.php">Epouvante-Horreur</a></li>
+                  <li><a href="Page.tarifs.php">Fantastique</a></li>
+                  <li><a href="Page.tarifs.php">Science-Fiction</a></li>
+                
+              </ul>
+          </header>
+          <hr/>
