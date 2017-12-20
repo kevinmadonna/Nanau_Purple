06e4c47b5eb431abce438abc37f15eae10f60cde
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-20 03:45:58 +0100

    oui

--- nanau-purple/js/script.js
+++ nanau-purple/js/script.js
@@ -1,7 +1,5 @@
 //alert("Bienvenue sur un site aussi oldschool que stylé !")
 
-
-
 document.getElementById("search-btn").onclick = function f1()
 {
   alert("form");
@@ -28,4 +26,33 @@ function LienAuHasard()
 
     ChoixLien = Math.floor(Math.random() * Url.length);
     window.open(base + Url[ChoixLien],'_blank');
+}
+
+*/
+
+var body = document.getElementsByTagName('body')[0],
+    img = document.createElement('img'),
+    imgTop = 100,
+    imgLeft = 100,
+    interval;
+
+// Ajoute une image à la page
+// Préférable à un appel à document.write()
+img.src = '/img/img_exemples/droite.gif';
+img.setAttribute('style', 'position:absolute;top:'+imgTop+'px;left:'+imgLeft+'px');
+body.appendChild(img);
+
+// Déplace l'image toutes les 100 millisecondes
+function move(x,y) {
+  interval = setInterval(function() {
+    imgTop += y;
+    imgLeft += x;
+    img.style.top = imgTop + 'px';
+    img.style.left = imgLeft + 'px';
+  }, 100);
+}
+
+// Stoppe le déplacement de l'image
+function stop() {
+  clearInterval(interval);
 }
