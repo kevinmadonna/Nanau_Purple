f8b830deee1b16d157f149e5284f9e5df50174ef
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-20 20:51:14 +0100

    enfiiiiin formulaire

--- nanau-purple/js/script.js
+++ nanau-purple/js/script.js
@@ -29,3 +29,20 @@ function LienAuHasard()
     ChoixLien = Math.floor(Math.random() * Url.length);
     window.open(base + Url[ChoixLien],'_blank');
 }
+
+function popup()
+{
+    width = 300;
+    height = 200;
+    if(window.innerWidth)
+    {
+        var left = (window.innerWidth-width)/2;
+        var top = (window.innerHeight-height)/2;
+    }
+    else
+    {
+        var left = (document.body.clientWidth-width)/2;
+        var top = (document.body.clientHeight-height)/2;
+    }
+    window.open('formulaire.html','Recherche avancée','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
+}
