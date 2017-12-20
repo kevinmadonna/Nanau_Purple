211d7db2ed46cd570c2a3e15f2bd0645842eed45
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-21 00:14:51 +0100

    fkkkhgf

--- nanau-purple/js/script.js
+++ nanau-purple/js/script.js
@@ -46,3 +46,37 @@ function popup()
     }
     window.open('formulaire.html','Recherche avancée','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
 }
+
+function popupinscription()
+{
+    width = 300;
+    height = 200;
+    if(window.innerWidth)
+    {
+        var left = (window.innerWidth-width)/2;
+        var top = (window.innerHeight-height)/2;
+    }
+    else
+    {
+        var left = (document.body.clientWidth-width)/2;
+        var top = (document.body.clientHeight-height)/2;
+    }
+    window.open('inscription.html','nscription','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
+}
+
+function popupconnexion()
+{
+    width = 300;
+    height = 200;
+    if(window.innerWidth)
+    {
+        var left = (window.innerWidth-width)/2;
+        var top = (window.innerHeight-height)/2;
+    }
+    else
+    {
+        var left = (document.body.clientWidth-width)/2;
+        var top = (document.body.clientHeight-height)/2;
+    }
+    window.open('connexion.html','Connexion','menubar=no, scrollbars=no, top='+top+', left='+left+', width='+width+', height='+height+'');
+}
