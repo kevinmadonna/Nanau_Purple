745d0963f090a89d09a03a8fc050ce97abe03ce9
MargauxVss <33689936+MargauxVss@users.noreply.github.com>
2017-12-20 02:17:38 +0100

    j'y arrive paaaaas

--- nanau-purple/js/script.js
+++ nanau-purple/js/script.js
@@ -12,3 +12,20 @@ function f1()
        alert("f1 called");
        //form validation that recalls the page showing with supplied inputs.
     }
+
+
+
+//bouton lien aléatoire
+
+function LienAuHasard()
+{
+    Url = new Array;
+    base = "file:///C:/Users/marga/OneDrive/Documents/GitHub/Nanau_Purple/nanau-purple/pages/";
+    Url[0] = "action.html";
+    Url[1] = "horreur.html";
+    Url[2] = "fantastique.html";
+    Url[3] = "fiction.html";
+
+    ChoixLien = Math.floor(Math.random() * Url.length);
+    window.open(base + Url[ChoixLien],'_blank');
+}
